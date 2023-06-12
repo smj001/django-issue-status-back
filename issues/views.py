74c3046529ed656fb31e94f15c7003671993c9aa
smj <s.m.javadi001@gmail.com>
2023-06-12 19:37:01 +0330

    add - add LatestIssueListAPIView to view

--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Issue
+from .serializers import IssueSerializer
 
-# Create your views here.
+class LatestIssueListAPIView(generics.ListAPIView):
+    queryset = Issue.objects.all().order_by('-submit_date')[:20]
+    serializer_class = IssueSerializer
