34a6f6b18972c3e18c9ba18270edf2636464f22f
smj <s.m.javadi001@gmail.com>
2023-06-12 19:33:29 +0330

    add - create models

--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Issue(models.Model):
+    subject = models.CharField(max_length=100)
+    description = models.TextField()
+    submit_date = models.DateTimeField(auto_now_add=True)
+    duration = models.PositiveIntegerField()
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.subject
